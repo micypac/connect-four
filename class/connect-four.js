57862c0dc41330d763194edc63ccae321458f83e
Michael Pacleb <michael.pacleb@gmail.com>
2022-10-13 10:48:31 -0400

    Implemented req to add checkWin logic in connect-four.js

--- class/connect-four.js
+++ class/connect-four.js
@@ -1,18 +1,19 @@
 const Screen = require("./screen");
 const Cursor = require("./cursor");
+const { winningArrays } = require("../utilities/utility");
 
 class ConnectFour {
-
   constructor() {
-
     this.playerTurn = "O";
 
-    this.grid = [[' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' ']]
+    this.grid = [
+      [" ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " "],
+    ];
 
     this.cursor = new Cursor(6, 7);
 
@@ -21,7 +22,7 @@ class ConnectFour {
     Screen.setGridlines(true);
 
     // Replace this with real commands
-    Screen.addCommand('t', 'test command (remove)', ConnectFour.testCommand);
+    Screen.addCommand("t", "test command (remove)", ConnectFour.testCommand);
 
     this.cursor.setBackgroundColor();
     Screen.render();
@@ -33,18 +34,52 @@ class ConnectFour {
   }
 
   static checkWin(grid) {
-
     // Return 'X' if player X wins
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
     // Return false if the game has not ended
 
+    const flatten = (grid) => {
+      let result = [];
+
+      for (let el of grid) {
+        if (Array.isArray(el)) result = result.concat(flatten(el));
+        else result.push(el);
+      }
+
+      return result;
+    };
+
+    let roundWon = false;
+    let playerWon = undefined;
+    let gameState = flatten(grid);
+
+    for (let i = 0; i < winningArrays.length; i++) {
+      const winComb = winningArrays[i];
+      let a = gameState[winComb[0]];
+      let b = gameState[winComb[1]];
+      let c = gameState[winComb[2]];
+      let d = gameState[winComb[3]];
+
+      if (a === " " || b === " " || c === " " || d === " ") continue;
+      if (a === b && b === c && c === d) {
+        roundWon = true;
+        playerWon = a;
+        break;
+      }
+    }
+
+    if (roundWon) return playerWon;
+
+    let roundDraw = !gameState.includes(" ");
+    if (roundDraw) return "T";
+    else return false;
   }
 
   static endGame(winner) {
-    if (winner === 'O' || winner === 'X') {
+    if (winner === "O" || winner === "X") {
       Screen.setMessage(`Player ${winner} wins!`);
-    } else if (winner === 'T') {
+    } else if (winner === "T") {
       Screen.setMessage(`Tie game!`);
     } else {
       Screen.setMessage(`Game Over`);
@@ -52,7 +87,6 @@ class ConnectFour {
     Screen.render();
     Screen.quit();
   }
-
 }
 
 module.exports = ConnectFour;
